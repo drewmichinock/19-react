cf4aae65c1b8748474ee5ce19125ec499b6ed13e
drewmichinock <drewmichinock@gmail.com>
2017-07-28 19:16:27 -0700

    commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// include the react dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// include the main parent component
+var Parent = require("./components/Parent");
+
+// This code here allows us to render our main component (in this case Parent)
+ReactDOM.render(<Parent />, document.getElementById("app"));
